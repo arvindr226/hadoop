021d723187525960c219c0a8116e9050fc5c3829
root <root@ip-172-31-32-22.us-west-2.compute.internal>
2017-04-16 13:16:56 +0000

    minor changes in docker-entry.sh

--- docker-entry.sh
+++ docker-entry.sh
@@ -13,15 +13,15 @@ rm /tmp/*.pid
 sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
 
 /usr/sbin/sshd -D &
-$HADOOP_PREFIX/sbin/start-all.sh
-#$HADOOP_PREFIX/sbin/start-dfs.sh
-#$HADOOP_PREFIX/sbin/start-yarn.sh
+#$HADOOP_PREFIX/sbin/start-all.sh
+$HADOOP_PREFIX/sbin/start-dfs.sh
+$HADOOP_PREFIX/sbin/start-yarn.sh
 #$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver
 
 #if [[ $1 == "-d" ]]; then
 #  while true; do sleep 1000; done
 #fi
 
-#if [[ $1 == "-bash" ]]; then
-#  /bin/bash
-#fi
+if [[ $1 == "-bash" ]]; then
+  /bin/bash
+fi
